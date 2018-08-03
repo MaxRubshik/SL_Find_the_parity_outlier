0f805094d440627980f1b41b7bd4adfce5a21183
Max Rubs <38310511+MaxRubshik@users.noreply.github.com>
2018-08-03 22:30:04 +0300

    Add files via upload
    I made it

---
+++ LS_SP_find_the_parity.py
@@ -0,0 +1,8 @@
+array_1 = [2, 4, 0, 100, 4, 11, 2602, 36]
+for n in array_1:
+    if n % 2 != 0:
+        print(n)
+array_2 = [160, 3, 1719, 19, 11, 13, -21]
+for m in array_2:
+    if m % 2 == 0:
+        print(m)
